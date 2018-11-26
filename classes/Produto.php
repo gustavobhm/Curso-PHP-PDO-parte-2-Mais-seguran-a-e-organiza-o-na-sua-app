2f59f5a7b00d8b2739af08d4611aea4c2e12e33a
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2018-11-26 08:26:24 -0200

    Aula 3 - Preparando script de banco de dados no PDO

--- classes/Produto.php
+++ classes/Produto.php
@@ -8,6 +8,28 @@ class Produto
     public $quantidade;
     public $categoria_id;
 
+    public function __construct($id = false)
+    {
+        if ($id) {
+            $this->id = $id;
+            $this->carregar();
+        }
+    }
+
+    public function carregar()
+    {
+        $query = "SELECT nome, preco, quantidade, categoria_id FROM produtos WHERE id = :id";
+        $conexao = Conexao::pegarConexao();
+        $stmt = $conexao->prepare($query);
+        $stmt->bindValue(':id', $this->id);
+        $stmt->execute();
+        $linha = $stmt->fetch();
+        $this->nome = $linha['nome'];
+        $this->preco = $linha['preco'];
+        $this->quantidade = $linha['quantidade'];
+        $this->categoria_id = $linha['categoria_id'];
+    }
+
     public static function listar()
     {
         $query = "SELECT p.id, p.nome, preco, quantidade, categoria_id, c.nome as categoria_nome
@@ -30,6 +52,28 @@ class Produto
         $stmt->bindValue(':preco', $this->preco);
         $stmt->bindValue(':quantidade', $this->quantidade);
         $stmt->bindValue(':categoria_id', $this->categoria_id);
+        $stmt->execute();
+    }
+
+    public function atualizar()
+    {
+        $query = "UPDATE produtos SET nome = :nome, preco = :preco, quantidade = :quantidade, categoria_id = :categoria_id WHERE id = :id";
+        $conexao = Conexao::pegarConexao();
+        $stmt = $conexao->prepare($query);
+        $stmt->bindValue(':nome', $this->nome);
+        $stmt->bindValue(':preco', $this->preco);
+        $stmt->bindValue(':quantidade', $this->quantidade);
+        $stmt->bindValue(':categoria_id', $this->categoria_id);
+        $stmt->bindValue(':id', $this->id);
+        $stmt->execute();
+    }
+
+    public function excluir()
+    {
+        $query = "DELETE FROM produtos WHERE id = :id";
+        $conexao = Conexao::pegarConexao();
+        $stmt = $conexao->prepare($query);
+        $stmt->bindValue('id', $this->id);
         $stmt->execute();
     }
 }
